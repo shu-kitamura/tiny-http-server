7dab1ca8c5423ca1b2271e7f0b8014987da25cf3
shu-kitamura <shusei3316@yahoo.co.jp>
2025-03-08 10:39:11 +0900

    パース関数を作成

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import socket
+import pprint
 
 def run_server(host='127.0.0.1', port=8080):
     # ソケットを作成
@@ -17,9 +18,9 @@ def run_server(host='127.0.0.1', port=8080):
         with client_conn:
             print(f'Connected by {client_addr}')
             # リクエストデータを受信（今回はシンプルに 1024 バイト）
-            request = client_conn.recv(1024)
-            decoded_request = request.decode('utf-8', errors='replace')
-            print(f"{decoded_request}")
+            request = client_conn.recv(1024)            
+            parse_request(request)
+
             
             # HTTP レスポンスの組み立て
             body = "Hello World"
@@ -32,6 +33,35 @@ def run_server(host='127.0.0.1', port=8080):
             )
             # レスポンスを送信
             client_conn.sendall(response.encode('utf-8'))
+
+def parse_request(request: bytes):
+    # parse HTTP request
+    result = {
+        "request_line": {
+            "method": "",
+            "path": "",
+            "protocol": ""},
+        "headers": {},
+        "body": ""
+    }
+    decoded_request = request.decode('utf-8')
+    splited_request = decoded_request.split('\r\n')
+    request_line = splited_request[0].split(" ")
+    if len(request_line) == 3:
+        result["request_line"]["method"] = request_line[0]
+        result["request_line"]["path"] = request_line[1]
+        result["request_line"]["protocol"] = request_line[2]
+    else:
+        print("Invalid Request")
+
+    for header in splited_request[1:]:
+        if header == "":
+            break
+        key, value = header.split(": ")
+        result["headers"][key] = value
+
+    pprint.pprint(result)    
+    
 
 if __name__ == '__main__':
     run_server()
