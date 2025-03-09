533e9fc76acb77645d8804cb3df96bb187d4064d
shu-kitamura <shusei3316@yahoo.co.jp>
2025-03-09 21:25:33 +0900

    ruff のルールを追加 & format, check を実行

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
-import socket
 import pprint
+import socket
 
-def run_server(host='127.0.0.1', port=8080):
+
+def run_server(host="127.0.0.1", port=8080):
     # ソケットを作成
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # アドレスの再利用を許可（サーバー再起動時の「Address already in use」回避）
@@ -10,18 +11,17 @@ def run_server(host='127.0.0.1', port=8080):
     server_socket.bind((host, port))
     # 接続の待ち受け開始
     server_socket.listen(5)
-    print(f'Starting server on {host}:{port}...')
+    print(f"Starting server on {host}:{port}...")
 
     while True:
         # クライアントからの接続を待機
         client_conn, client_addr = server_socket.accept()
         with client_conn:
-            print(f'Connected by {client_addr}')
+            print(f"Connected by {client_addr}")
             # リクエストデータを受信（今回はシンプルに 1024 バイト）
-            request = client_conn.recv(1024)            
+            request = client_conn.recv(1024)
             parse_request(request)
 
-            
             # HTTP レスポンスの組み立て
             body = "Hello World"
             response = (
@@ -32,20 +32,18 @@ def run_server(host='127.0.0.1', port=8080):
                 f"{body}"
             )
             # レスポンスを送信
-            client_conn.sendall(response.encode('utf-8'))
+            client_conn.sendall(response.encode("utf-8"))
+
 
 def parse_request(request: bytes):
     # parse HTTP request
     result = {
-        "request_line": {
-            "method": "",
-            "path": "",
-            "protocol": ""},
+        "request_line": {"method": "", "path": "", "protocol": ""},
         "headers": {},
-        "body": ""
+        "body": "",
     }
-    decoded_request = request.decode('utf-8')
-    splited_request = decoded_request.split('\r\n')
+    decoded_request = request.decode("utf-8")
+    splited_request = decoded_request.split("\r\n")
     request_line = splited_request[0].split(" ")
     if len(request_line) == 3:
         result["request_line"]["method"] = request_line[0]
@@ -60,8 +58,8 @@ def parse_request(request: bytes):
         key, value = header.split(": ")
         result["headers"][key] = value
 
-    pprint.pprint(result)    
-    
+    pprint.pprint(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_server()
